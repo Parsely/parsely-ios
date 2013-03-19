937cb7d59eb4ee88ce088b1d89ffcea186c19cc3
Emmett Butler <emmett.butler321@gmail.com>
2013-03-19 10:42:27 -0400

    Persist the queue before dropping events

--- PixelTester/ParselyiOS/ParselyTracker.m
+++ PixelTester/ParselyiOS/ParselyTracker.m
@@ -45,6 +45,7 @@ ParselyTracker *instance;
     
     if([self queueSize] >= [self queueSizeLimit] + 1){
         PLog(@"Queue size exceeded, expelling event to persistent memory");
+        [self persistQueue];
         [eventQueue removeObjectAtIndex:0];
     }
     
@@ -101,10 +102,7 @@ ParselyTracker *instance;
     [request setHTTPMethod:@"GET"];
     NSError *requestError;
     NSURLResponse *urlResponse = nil;
-#ifdef PARSELY_DEBUG
-#else
     [NSURLConnection sendSynchronousRequest:request returningResponse:&urlResponse error:&requestError];
-#endif
     PLog(@"Sent request to %@", url);
 }
 
