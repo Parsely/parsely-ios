4bb423cb8220d66a0b81ca62714a2d6724d59962
Emmett Butler <emmett.butler321@gmail.com>
2013-03-21 15:17:37 -0400

    change documentation target

--- Documentation/html/search/all_72.js
+++ Documentation/html/search/all_72.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['readme',['README',['../md__r_e_a_d_m_e.html',1,'']]],
   ['rooturl',['rootUrl',['../interface_parsely_tracker.html#abbf5a64e4810b30e6a483d8696563b3d',1,'ParselyTracker']]]
 ];
