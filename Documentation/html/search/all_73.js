e5a2cc838a9e6dcfb09e0c7e8e5b6217548bd140
Emmett Butler <emmett.butler321@gmail.com>
2013-03-22 13:09:45 -0400

    docs update

--- Documentation/html/search/all_73.js
+++ Documentation/html/search/all_73.js
@@ -1,10 +1,10 @@
 var searchData=
 [
   ['sendbatchrequest_3a',['sendBatchRequest:',['../interface_parsely_tracker.html#a982f5ed908f98a6829eb2a37ac57f681',1,'ParselyTracker']]],
+  ['setflushtimer',['setFlushTimer',['../interface_parsely_tracker.html#a41c0b0696844f4b48b333dcd96dedbbb',1,'ParselyTracker']]],
   ['sharedinstance',['sharedInstance',['../interface_parsely_tracker.html#ac253d0fd6a9781b1a4d65012400d5a80',1,'ParselyTracker']]],
   ['sharedinstancewithapikey_3a',['sharedInstanceWithApiKey:',['../interface_parsely_tracker.html#a05b5cbcf2cdf6ef3f29b130176045bd3',1,'ParselyTracker']]],
   ['shouldflushonbackground',['shouldFlushOnBackground',['../interface_parsely_tracker.html#a8ca7d8ee30b8c05c774f380529980e88',1,'ParselyTracker']]],
-  ['start',['start',['../interface_parsely_tracker.html#a47a4f81be19440871e3507959df0df4e',1,'ParselyTracker']]],
-  ['stop',['stop',['../interface_parsely_tracker.html#af152495da9a904f96ab34b1a703a2eb1',1,'ParselyTracker']]],
+  ['stopflushtimer',['stopFlushTimer',['../interface_parsely_tracker.html#a85cbb9a10717f53c609697d9528ac482',1,'ParselyTracker']]],
   ['storedeventscount',['storedEventsCount',['../interface_parsely_tracker.html#a90110859528d8c6da8678a793faadb72',1,'ParselyTracker']]]
 ];
