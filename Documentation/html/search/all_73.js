2f65bc59a26cd2302716c5930cdb8b58ad77e96e
Emmett Butler <emmett.butler321@gmail.com>
2013-04-22 14:54:42 -0400

    update documentation

--- Documentation/html/search/all_73.js
+++ Documentation/html/search/all_73.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['sendbatchrequest_3a',['sendBatchRequest:',['../interface_parsely_tracker.html#a982f5ed908f98a6829eb2a37ac57f681',1,'ParselyTracker']]],
+  ['sendbatchrequest_3a',['sendBatchRequest:',['../interface_parsely_tracker.html#ac92a7e5ce014ecf1bafbb6d694f5ac07',1,'ParselyTracker']]],
   ['setflushtimer',['setFlushTimer',['../interface_parsely_tracker.html#a41c0b0696844f4b48b333dcd96dedbbb',1,'ParselyTracker']]],
   ['sharedinstance',['sharedInstance',['../interface_parsely_tracker.html#ac253d0fd6a9781b1a4d65012400d5a80',1,'ParselyTracker']]],
   ['sharedinstancewithapikey_3a',['sharedInstanceWithApiKey:',['../interface_parsely_tracker.html#a05b5cbcf2cdf6ef3f29b130176045bd3',1,'ParselyTracker']]],
